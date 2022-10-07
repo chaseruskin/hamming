4c7c63e26cc25882d6d167c3117c1ad2553e67a8
Chase Ruskin <c.ruskin@ufl.edu>
2022-10-07 10:22:23 -0400

    tests sw parity bit function

--- sim/mdl/hamming.py
+++ sim/mdl/hamming.py
@@ -62,13 +62,16 @@ def display(block: List[int], width=None, end='\n'):
     pass
 
 
-def set_parity_bit(arr: List[int]) -> bool:
+def set_parity_bit(arr: List[int], use_even=True) -> bool:
     '''
-    Checks if the `arr` is odd parity in which case the parity bit
+    Checks if the `arr` has an odd amount of 0's in which case the parity bit
     must be set to '1' to achieve an even parity.
+
+    If `use_even` is set to `False`, then odd parity will be computed and will
+    seek to achieve an odd amount of '1's (including parity bit).
     '''
     # count the number of 1's in the list
-    return arr.count(1) % 2
+    return (arr.count(1) % 2) ^ (use_even == False)
 
 
 def _binary_space(n: int) -> List[str]:
@@ -342,6 +345,23 @@ class TestHammingEcc(unittest.TestCase):
 
 
     def test_get_parity_coverage(self):
+        pass
+
+
+    def test_compute_parity(self):
+        # even parity
+        check = set_parity_bit([1, 0, 0])
+        self.assertEqual(check, 1)
+
+        check = set_parity_bit([1, 0, 0, 1])
+        self.assertEqual(check, 0)
+
+        # odd parity
+        check = set_parity_bit([1, 0, 0, 1], use_even=False)
+        self.assertEqual(check, 1)
+
+        check = set_parity_bit([1, 0, 1, 1], use_even=False)
+        self.assertEqual(check, 0)
         pass
 
     pass
